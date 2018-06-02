230cc08a85a4316a7527697fb578fc7bc2aeb306
Dararith Khel <kheldararith@outlook.com>
2018-06-02 11:11:49 +0700

    Edit code

--- himaven/src/main/java/javamaven/himaven/App.java
+++ himaven/src/main/java/javamaven/himaven/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello Please welcome to GitHub!" );
+        System.out.println("Hey Bros");
     }
 }
